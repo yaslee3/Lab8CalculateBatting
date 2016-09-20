afcd4702cfcfa32979035c78699a09898e8659ab
Yasmine Lee <yaslee3@yahoo.com>
2016-09-20 17:21:32 -0400

    initial commit comment

---
+++ src/Lab8/BatAverages.java
@@ -0,0 +1,34 @@
+package Lab8;
+
+public class BatAverages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Create a Scanner
+		
+		//print welcome message to console
+		
+		
+		//Add decimal Format so the three decimal places are shown
+		
+		
+		//declare variables
+
+
+		//Prompt user to enter number of times at bat 
+		
+		
+		//for loop to take in the timesAtBat 
+		
+		
+		// the user can only enter 0,1,2,3,or 4
+		
+		
+		//String to only take in entry Y,y,N,n 
+		
+		
+		//end program it the user chooses N or n,
+	}
+
+}
